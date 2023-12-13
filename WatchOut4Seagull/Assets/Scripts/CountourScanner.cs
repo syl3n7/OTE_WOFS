3c54cc746ae74ddd88579499a3c1f6fee5848b67
syl3n7 <claudiosilva@protonmail.com>
2023-12-13 13:23:14 +0000

    Vision Compute done.

--- WatchOut4Seagull/Assets/Scripts/CountourScanner.cs
+++ WatchOut4Seagull/Assets/Scripts/CountourScanner.cs
@@ -11,29 +11,38 @@ public class CountourScanner : WebCamera
     private Mat image;
     //import the player gameobject and control it using any yellow blob that appears on the output, if there is no blobs, the player will not move
     public GameObject player;
-    protected override bool ProcessTexture(WebCamTexture input, ref Texture2D output)
+    protected override bool ProcessTexture(WebCamTexture input, ref Texture2D output)                             
     {
         image = OpenCvSharp.Unity.TextureToMat(input);
 
-        Cv2.CvtColor(image, image, ColorConversionCodes.BGR2GRAY);
-        Cv2.Threshold(image, image, 127, 255, ThresholdTypes.Binary);
-        Cv2.FindContours(image, out var contours, out var hierarchy, RetrievalModes.Tree, ContourApproximationModes.ApproxSimple);
-        CascadeClassifier handCascade = new CascadeClassifier("hand.xml");
+        //Cv2.CvtColor(image, image, ColorConversionCodes.BGR2GRAY);
+        //Cv2.Threshold(image, image, 127, 255, ThresholdTypes.Binary);
+        //Cv2.FindContours(image, out var contours, out var hierarchy, RetrievalModes.Tree, ContourApproximationModes.ApproxSimple);
+
+        CascadeClassifier handCascade = new CascadeClassifier("Assets/rpalm.xml");
         Mat frame = OpenCvSharp.Unity.TextureToMat(input);
-        Mat gray = new Mat();
-        Cv2.CvtColor(frame, gray, ColorConversionCodes.BGR2GRAY);
-        Rect[] hands = handCascade.DetectMultiScale(gray, 1.1, 2, HaarDetectionType.ScaleImage, new Size(30, 30));
+        //Mat gray = new Mat();
+        //Cv2.CvtColor(frame, gray, ColorConversionCodes.BGR2GRAY);
+        Rect[] hands = handCascade.DetectMultiScale(frame, 1.1, 2, HaarDetectionType.ScaleImage, new Size(30, 30));
 
-        float handX = hands[0].X + hands[0].Width / 2;
-        float handY = hands[0].Y + hands[0].Height / 2;
+        if (hands.Length > 0)
+        {
+            //Vec3b color = frame.At<Vec3b>(hands[0].Y + hands[0].Height / 2, hands[0].X + hands[0].Width / 2);
+            //Debug.Log(color.Item2);
+            //if (color.Item2 > 10) // Check if color is yellow
+            //{
+                float handX = hands[0].X + hands[0].Width / 2;
+                float handY = hands[0].Y + hands[0].Height / 2;
 
-        float playerX = handX / frame.Width * Screen.width;
-        float playerY = handY / frame.Height * Screen.height;
+                float playerX = handX / frame.Width * Screen.width;
+                float playerY = handY / frame.Height * Screen.height;
 
-        player.transform.position = new Vector2(playerX, playerY);
-        Cv2.DrawContours(image, contours, -1, Scalar.Red, 2);
+                player.transform.position = new Vector2(playerX, playerY);
+            //}
+        }
+
+        //Cv2.DrawContours(image, contours, -1, Scalar.Red, 2);
         Cv2.ImShow("output", image);
-
 
         if (output == null)
         {

c7b4608292a2f4201953608e2ded217bd3059120
Mariana041 <100358775+Mariana041@users.noreply.github.com>
2023-12-13 00:53:22 +0000

    Main Menu Buttons

--- WatchOut4Seagull/Assets/Scripts/Menu.cs
+++ WatchOut4Seagull/Assets/Scripts/Menu.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     public void PlayGame() => SceneManager.LoadScene("In-Game");
+    public void Instructions() => SceneManager.LoadScene("Instructions");
+    public void Back() => SceneManager.LoadScene("Menu");
 
     public void QuitGame() => Application.Quit();
 }
